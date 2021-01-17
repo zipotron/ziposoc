381d1af8c77d9a94c28ee77aee6ea9dcacb911fd
zipotron <zipotron@localhost.localdomain>
2021-01-17 21:17:45 +0200

    ALIGN RAM exception implemented

--- ram.v
+++ ram.v
@@ -18,13 +18,13 @@ module ram
 		end
 	end
 	
-	assign read = exception | rw? 0: ram_array[addr];//exception | rw[2]? 0 : ~|rw_len[1:0]? {ram_array[addr], 24'b0} : ~rw_len[2] & ~rw_len[1] & rw_len[0]? {ram_array[addr],ram_array[addr+1], 16'b0} : ~rw_len[2] & rw_len[1] & ~rw_len[0]? {ram_array[addr],ram_array[addr+1],ram_array[addr+2],ram_array[addr+3]} : 0;
+	assign read = exception | rw? 0 : ~|len[1:0]? {24'b0, ram_array[addr]} : ~len[1] & len[0]? {16'b0, ram_array[addr+1], ram_array[addr]} : len[1] & ~len[0]? {ram_array[addr+3],ram_array[addr+2],ram_array[addr+1],ram_array[addr]} : 0;
 
-	assign exception = |addr[31:ram_width + 1]? 1: 0;//~|rw_len[1:0]? 0: ~rw_len[1] & rw_len[0] &addr[0]? 0: rw_len[1] & ~rw_len[0] & ~|addr[1:0]? 0: 1;
+	assign exception = ~|len[1:0]? 0: ~len[1] & len[0] &addr[0]? 0: len[1] & ~len[0] & ~|addr[1:0]? 0: 1;
 
 	always @(posedge clk) begin
 		if (rw & ~exception) begin
-			/*case (rw_len[1:0])
+			/*case (len[1:0])
 				2'b00:*/
 					ram_array[addr] <= write;
 				/*2'b01: begin
