d2fe9d553b3bc9660f7f88a9a76c5d4e99d75ce5
zipotron <zipotron@localhost.localdomain>
2021-03-26 10:15:05 +0200

    Merge from ulx3s

---
+++ memory_map.v
@@ -0,0 +1,28 @@
+/* CSR */
+`define CSR_RANGE 31:6
+/* 0 .. 0x40  64bytes*/
+`define MSTATUS 32'h00000000 //4 bytes Machine status
+`define MCAUSE 32'h00000004 //4 bytes interruption or exception cause
+`define MTIME 32'h00000008 //8 bytes
+`define MTIMECMP 32'h00000010 //8 bytes
+//`define MIP 32'h00000024 //4 bytes Pending interrupts
+`define MTVEC 32'h00000018 //4 bytes base address of interrupt handler (Direct)
+`define MEPC 32'h0000001C //4 bytes Return adress after interrupt
+`define MSCRATCH 32'h00000020 //4 bytes Can be used a Stack pointer for interruption processing
+
+/* data section b'1000000*/
+`define DATA_RANGE 31:12
+`define DATA_TOKEN 6:11
+/* 0x40 .. 0x1000*/
+`define RAM_INIT 32'h00000040
+`define INITIAL_PC 32'h00000040
+`define INITIAL_SP 32'h00000800
+
+/* mapped IO section b'1000000000000*/
+`define IO_RANGE 31:14
+`define IO_TOKEN 12:13
+/* 0x1000 */
+`define IO_INIT 32'h00001000
+`define LEDS 32'h00001000
+
+`define MEM_END 32'h00001080
